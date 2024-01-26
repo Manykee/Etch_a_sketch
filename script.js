fcc140c60b50d07b0558991b3111c9bfb269f630
Manykee <cristian.virlan@gmail.com>
2024-01-26 20:06:05 +0200

    Added the collor selector working, create the div's using the inputed number not working

--- script.js
+++ script.js
@@ -1,8 +1,49 @@
 let number = document.getElementById("inputedNumber");
 let range = document.getElementById("range");
 
+
 range.addEventListener("pointermove", inputChange);
 
 function inputChange() {
     number.innerText = range.value;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*function createDiv(number) {
+    const container = document.getElementById("drawBoard");
+    for (let i = 0; i < number; i++) {
+        const newDiv = document.createElement("div");
+        newDiv.setAttribute("class", "innerDiv");
+        container.appendChild(newDiv);
+    };
+};*/
