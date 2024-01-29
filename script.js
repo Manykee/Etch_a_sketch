f4533231cd8a456b829653d95023f32381c974af
Manykee <cristian.virlan@gmail.com>
2024-01-29 18:16:44 +0200

    Working function for number of divs, not working css for innerdiv

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
-let number = document.getElementById("inputedNumber");
-let range = document.getElementById("range");
+let number = document.querySelector("#inputedNumber");
+let range = document.querySelector("#range");
+const container = document.querySelector("#drawBoard");
 
 
 range.addEventListener("pointermove", inputChange);
@@ -7,43 +8,24 @@ range.addEventListener("pointermove", inputChange);
 function inputChange() {
     number.innerText = range.value;
 }
+range.addEventListener("change", divNumber);
 
+function divNumber() {
+    removeAllChild(container);
+    createDiv((range.value)**2);
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*function createDiv(number) {
-    const container = document.getElementById("drawBoard");
+function createDiv(number) {
+    
     for (let i = 0; i < number; i++) {
         const newDiv = document.createElement("div");
         newDiv.setAttribute("class", "innerDiv");
         container.appendChild(newDiv);
     };
-};*/
+};
+
+function removeAllChild(parent) {
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild);
+    }
+}
