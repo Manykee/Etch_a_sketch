4ea1ded78a19e55db8cf56de1868481b242cc9e9
Manykee <cristian.virlan@gmail.com>
2024-01-31 21:14:17 +0200

    Working div creator and size adjusting to the number of divs

--- script.js
+++ script.js
@@ -1,18 +1,20 @@
 let number = document.querySelector("#inputedNumber");
 let range = document.querySelector("#range");
 const container = document.querySelector("#drawBoard");
-
+const rangeNumber = range.value.toString;
 
 range.addEventListener("pointermove", inputChange);
 
 function inputChange() {
     number.innerText = range.value;
+    
 }
 range.addEventListener("change", divNumber);
 
 function divNumber() {
     removeAllChild(container);
     createDiv((range.value)**2);
+    divSizeAllocation(range.value);
 }
 
 function createDiv(number) {
@@ -23,6 +25,15 @@ function createDiv(number) {
         container.appendChild(newDiv);
     };
 };
+    
+
+function divSizeAllocation(number) {
+    for(let i = 0; i < number**2; i++) {
+        const divSize = document.querySelectorAll(".innerDiv");
+        divSize[i].style.width = 500 / number + "px";
+        divSize[i].style.height = 500 / number + "px";
+    }
+}
 
 function removeAllChild(parent) {
     while (parent.firstChild) {
